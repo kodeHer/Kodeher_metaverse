52a9d1de75eccbfbcaf13e5f602a665a51590eb8
Theophilus Arthur <91135692+Theoa1@users.noreply.github.com>
2023-05-05 16:14:29 -0500

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.jpg',
+    title: 'Join The CYBERPUNK ',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.jpg',
+    title: 'The IllusLi0n5',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.jpg',
+    title: 'WAR CHAMPS',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.jpg',
+    title: 'The Visionary',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.jpg',
+    title: 'Meta Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.jpg',
+    title: 'Metaverse Launch Leaves Elon Musk on the Edge of His Seat',
+    subtitle:
+        'The recent launch of the Metaverse has sent shockwaves throughout the tech industry, leaving even the likes of Elon Musk in awe...',
+  },
+  {
+    imgUrl: '/planet-07.jpg',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Get familiar with the technology: Start by understanding the basics of virtual reality and augmented reality technology...',
+  },
+  {
+    imgUrl: '/planet-08.jpg',
+    title: 'With one platform you can explore the whole world of art virtually',
+    subtitle:
+        'Art enthusiasts around the world can now explore the whole world of art from the comfort of their homes. A new platform has been...',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About MetaWorld" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">KODEHER METAWORLD</span> i is the future of gaming and promises to revolutionize the way we experience virtual worlds.
+
+        The Metaverse is more than just a game or a simulation, it's a place where you can live out your wildest dreams and explore limitless possibilities. 
+        because this is really the{' '}
+        <span className="font-extrabold text-white">
+          madness of the metaWORLD
+        </span>{' '}
+        of today, using only{' '}
+        <span className="font-extrabold text-white">VR</span> devices you can
+        easily explore the metaverse world you want, turn your dreams into
+        reality. Let's{' '}
+        <span className="font-extrabold text-white">explore</span> the madness
+        of the metaverse by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
